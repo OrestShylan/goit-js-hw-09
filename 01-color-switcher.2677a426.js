2a218438ba95d3372d103efba5b744e3ae5fb615
OrestShylan <112808022+OrestShylan@users.noreply.github.com>
2023-02-03 21:09:20 +0000

    Deploying to gh-pages from @ OrestShylan/goit-js-hw-09@c922f3322c6d46cf998226cd250a1c5ca52abff0 🚀

---
+++ 01-color-switcher.2677a426.js
@@ -0,0 +1,2 @@
+!function(){var t={start:document.querySelector("[data-start]"),stop:document.querySelector("[data-stop]"),body:document.querySelector("body")},e=null;t.start.addEventListener("click",(function(o){e=setInterval((function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.start.disabled=!0,t.stop.disabled=!1})),t.stop.addEventListener("click",(function(o){clearInterval(e),t.start.disabled=!1,t.stop.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.2677a426.js.map
